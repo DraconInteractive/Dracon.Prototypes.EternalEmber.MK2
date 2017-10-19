c558ff0f14f50cd409f4005b2020316efb8c475e
RarCeth <rarceth@gmail.com>
2017-10-19 19:37:30 +0800

    custom blades and crafting table in farm

--- Eternal Ember MK-II/Assets/Scripts/Item_Type.cs
+++ Eternal Ember MK-II/Assets/Scripts/Item_Type.cs
@@ -218,35 +218,6 @@ public class Inventory
     }
 }
 
-[CreateAssetMenu(fileName = "Item Controller", menuName = "Generate/Item Controller", order = 0)]
-public class ItemController : ScriptableObject
-{ 
-    public List<Item_Type> allItems = new List<Item_Type> ();
-    public List<Item> itemEquiv = new List<Item> ();
-    public Item_Type searchedItem = null;
-
-    public void SearchForItem (string search)
-    {
-        Item_Type it = null;
-        foreach (Item_Type i in allItems)
-        {
-            if (i.itemName == search)
-            {
-                it = i;
-                break;
-            }
-        }
-        if (it != null)
-        {
-            searchedItem = it;
-        }
-        else
-        {
-            searchedItem = null;
-        }
-        //searchedItem = new Item ("null", null, null, Item.ItemType.Consumable);
-    }
-}
 
 
 
