22695ce2bd4d348bd1da4b0d924e9ac706883450
RarCeth <rarceth@gmail.com>
2017-12-06 20:13:44 +0800

    advanced item container implementation

---
+++ Eternal Ember MK-II/Assets/ItemContainerAdvanced.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemContainerAdvanced : ItemContainer {
+	public Dictionary <Item_Type, ItemCount> itemDictionary;
+	public List<ItemCount> quantities = new List<ItemCount>();
+	// Use this for initialization
+	void Start () {
+		itemDictionary = new Dictionary<Item_Type, ItemCount> ();
+		for (int i = 0; i < itemsInContainer.Count; i++) {
+			itemDictionary.Add (itemsInContainer [i], quantities [i]);
+		}
+	}
+}
+[Serializable]
+public class ItemCount {
+	public enum CountType {SingleNumber, RandomNumber, NumberRange, RandomChoice};
+	public CountType count;
+
+	public int single;
+	public int rangeMin;
+	public int rangeMax;
+	public List<int> choices = new List<int>();
+
+	public int GetResult () {
+		int res = 0;
+		switch (count)
+		{
+		case CountType.SingleNumber:
+			res = single;
+			break;
+		case CountType.RandomNumber:
+			res = UnityEngine.Random.Range (0, 100);
+			break;
+		case CountType.NumberRange:
+			res = UnityEngine.Random.Range (rangeMin, rangeMax);
+			break;
+		case CountType.RandomChoice:
+			int r = UnityEngine.Random.Range (0, choices.Count);
+			res = choices [r];
+			break;
+		}
+
+		return res;
+	}
+}
--- Eternal Ember MK-II/Assets/Scripts/ItemContainer.cs
+++ Eternal Ember MK-II/Assets/Scripts/ItemContainer.cs
@@ -7,6 +7,7 @@ using DuloGames.UI;
 public class ItemContainer : MonoBehaviour
 {
     public List<Item_Type> itemsInContainer;
+	public bool interactable;
 
     public void AddToInventory ()
     {
@@ -17,9 +18,45 @@ public class ItemContainer : MonoBehaviour
 			p_Inventory.AddItem(new Item(item, 1));
         }
 		InventoryCanvas.invCanvas.PopulateInventorySlots ();
-        GetComponent<Interactable>().uiWindow.Hide();
+		if (interactable) {
+			GetComponent<Interactable>().uiWindow.Hide();
+		}
+        
         Destroy(this);
     }
+
+	public void AddToInventory (List<Item_Type> items) {
+		Inventory p_Inventory = Player.player.playerInventory;
+		print ("There are " + items.Count + " items in container");
+		foreach (Item_Type item in items)
+		{
+			p_Inventory.AddItem(new Item(item, 1));
+		}
+		InventoryCanvas.invCanvas.PopulateInventorySlots ();
+		if (interactable) {
+			GetComponent<Interactable>().uiWindow.Hide();
+		}
+
+		Destroy(this);
+	}
+
+	public void AddToInventory (Dictionary<Item_Type, int> items) {
+		Inventory p_Inventory = Player.player.playerInventory;
+		print ("There are " + items.Keys.Count + " items in container");
+//		foreach (Item_Type item in items)
+//		{
+//			p_Inventory.AddItem(new Item(item, 1));
+//		}
+		foreach (KeyValuePair <Item_Type, int> pair in items) {
+			p_Inventory.AddItem (new Item (pair.Key, pair.Value));
+		}
+		InventoryCanvas.invCanvas.PopulateInventorySlots ();
+		if (interactable) {
+			GetComponent<Interactable>().uiWindow.Hide();
+		}
+
+		Destroy(this);
+	}
 }
 
 
